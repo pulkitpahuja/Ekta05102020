9ecfdfad7edb9d8b1245c0842993f5eaec821d58
Pulkit Pahuja <pulkit.pahuja98@gmail.com>
2022-02-27 21:53:21 +0530

    value fixes

--- Ekta-27-02-2022-v3.1/app.py
+++ Ekta-27-02-2022-v3.1/app.py
@@ -93,8 +93,8 @@ def download_this_csv(result_data, name):
 
 def compute_float(bytes_rec):
     data = []
-    bytes_rec.pop()
-    bytes_rec.pop()  ## deletes last 2 bytes (Checksum)
+    del bytes_rec[-1]
+    del bytes_rec[-1]  ## deletes last 2 bytes (Checksum)
     del bytes_rec[:3]  ## deletes first 3 bytes (Header)
     for i in range(0, len(bytes_rec), 4):
         list1 = [bytes_rec[i + 1], bytes_rec[i], bytes_rec[i + 3], bytes_rec[i + 2]]
